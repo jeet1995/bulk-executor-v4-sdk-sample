7cbd88b0f547ea58de46d1e29a98586b6dcd8950
Abhijeet Mohanty <abhmohanty@microsoft.com>
2024-12-17 21:55:43 -0500

    Added cosmos diagnostics logging filter.

--- src/main/java/org/example/logging/CosmosDiagnosticsLoggingThrottler.java
+++ src/main/java/org/example/logging/CosmosDiagnosticsLoggingThrottler.java
@@ -1,38 +1,31 @@
 package org.example.logging;
 
+import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.filter.Filter;
 import ch.qos.logback.core.spi.FilterReply;
-import org.slf4j.event.LoggingEvent;
-
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class CosmosDiagnosticsLoggingThrottler extends Filter<LoggingEvent> {
+public class CosmosDiagnosticsLoggingThrottler extends Filter<ILoggingEvent> {
 
-    private final static int COUNT_THRESHOLD = 20;
+    private final static int COUNT_THRESHOLD = 10;
     private final static long LOG_INTERVAL = 60_000;
 
     private final AtomicInteger count = new AtomicInteger(0);
     private volatile long lastLogTime = System.currentTimeMillis();
 
     @Override
-    public FilterReply decide(LoggingEvent loggingEvent) {
+    public FilterReply decide(ILoggingEvent loggingEvent) {
         if (loggingEvent.getLoggerName().contains("CosmosDiagnosticsLogger")) {
 
             if (count.get() < COUNT_THRESHOLD) {
-                System.out.println("Count threshold satisfied");
                 count.incrementAndGet();
                 lastLogTime = System.currentTimeMillis();
                 return FilterReply.ACCEPT;
             } else if (System.currentTimeMillis() - lastLogTime > LOG_INTERVAL)  {
-
-                System.out.println("Count threshold not satisfied but time window threshold satisfied");
                 count.set(1);
                 lastLogTime = System.currentTimeMillis();
                 return FilterReply.ACCEPT;
             } else {
-
-                System.out.println("Count threshold not satisfied and time window threshold not satisfied");
-
                 return FilterReply.DENY;
             }
         }
